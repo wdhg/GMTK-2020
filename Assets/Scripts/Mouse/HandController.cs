22c9a3f5d8f80359bbc4b9ff750c772f38062bc8
wdhg <wdhgrant@gmail.com>
2020-07-11 16:44:33 +0100

    add basic hand

---
+++ Assets/Scripts/Mouse/HandController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class HandController : MonoBehaviour {
+
+  public float maxSpeed;
+
+  private void Move() {
+    Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    this.transform.position = Vector2.MoveTowards(this.transform.position, mousePos, maxSpeed * Time.deltaTime);
+  }
+
+  public void Update() {
+    Move();
+  }
+}
