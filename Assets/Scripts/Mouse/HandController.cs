3acf0c1b408a274f89192f384d8f18964c099352
wdhg <wdhgrant@gmail.com>
2020-07-11 17:01:47 +0100

    add shake

--- Assets/Scripts/Mouse/HandController.cs
+++ Assets/Scripts/Mouse/HandController.cs
@@ -1,18 +1,31 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UIElements;
 
 public class HandController : MonoBehaviour {
 
   public float maxSpeed;
+
+  private bool isShaking = true;
+  private float shakeAmount = 5f;
+  private float shakeAngle = 0f;
 
   private void Move() {
     Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
     this.transform.position = Vector2.MoveTowards(this.transform.position, mousePos, maxSpeed * Time.deltaTime);
   }
 
+  private void Shake() {
+    shakeAngle += UnityEngine.Random.Range((float) -Math.PI / 2f, (float) Math.PI / 2f);
+    Vector2 dir = new Vector2((float) Math.Cos(shakeAngle), (float) Math.Sin(shakeAngle));
+    this.transform.position += (Vector3) dir * shakeAmount * Time.deltaTime;
+  }
+
   public void Update() {
     Move();
+    if(isShaking) {
+      this.Shake();
+    }
   }
 }
