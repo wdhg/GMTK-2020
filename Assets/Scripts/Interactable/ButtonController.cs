7f5325e35ea9a7a9ca79432eb6808ebc4d75e628
wdhg <wdhgrant@gmail.com>
2020-07-11 23:21:45 +0100

    make buttons click via hand not mouse

--- Assets/Scripts/Interactable/ButtonController.cs
+++ Assets/Scripts/Interactable/ButtonController.cs
@@ -1,14 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class ButtonController : MonoBehaviour {
 
   public string id;
 
-  public void OnMouseDown() {
-    if(Config.DEBUG) {
-      Debug.Log("Button " + id + " pressed");
+  bool handHovering = false;
+
+  private void OnTriggerEnter2D(Collider2D collision) {
+    if(collision.tag == "Hand") {
+      this.handHovering = true;
     }
   }
+
+  private void OnTriggerExit2D(Collider2D collision) {
+    if(collision.tag == "Hand") {
+      this.handHovering = false;
+    }
+  }
+
+  private void Update() {
+    if(this.handHovering && Input.GetKeyDown(KeyCode.Mouse0)) {
+      if(Config.DEBUG) {
+        Debug.Log("Button " + id + " pressed");
+      }
+    }
+  }
+
 }
