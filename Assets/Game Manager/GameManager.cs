39976a78832149206062da5d3e254372355787c2
wdhg <wdhgrant@gmail.com>
2020-07-12 18:44:50 +0100

    gameover stuff

--- Assets/Game Manager/GameManager.cs
+++ Assets/Game Manager/GameManager.cs
@@ -1,18 +1,36 @@
 ﻿using System.Threading;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour {
 
   public TextMeshProUGUI timerText;
+  public GameObject ui, gameOver;
+  public Meter sanity, work;
+  public Text timeText;
 
   private float time;
+  private bool isGameOver;
 
   private void Update() {
     if(Input.GetKeyDown(KeyCode.Escape)) {
       Application.Quit();
     }
-    time += Time.deltaTime;
-    timerText.text = "Timer - " + (Mathf.Round(time * 100) / 100).ToString();
+    if (isGameOver) {
+      if (Input.anyKeyDown) {
+        SceneManager.LoadScene(0); // TODO set final scene number here
+      }
+    } else {
+      time += Time.deltaTime;
+      timerText.text = "Timer - " + (Mathf.Round(time * 100) / 100).ToString();
+      if(sanity.currentValue <= 0 || work.currentValue <= 0) {
+        ui.SetActive(false);
+        gameOver.SetActive(true);
+        timeText.text = "Time - " + (Mathf.Round(time * 100) / 100).ToString();
+        this.isGameOver = true;
+      }
+    }
   }
 }
