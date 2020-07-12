37ee30517d42594223c034b6c5c4c4c60c783e96
Hana <hannah@tnsc.co.uk>
2020-07-12 19:12:44 +0100

    Final build

--- Assets/Scripts/Meter/Meter.cs
+++ Assets/Scripts/Meter/Meter.cs
@@ -5,13 +5,18 @@ public class Meter : MonoBehaviour {
     float maxValue = 1.0f;
     float minValue = 0.0f;
     public float currentValue = 0.5f;
-    public float timePerDecrement = 1.0f;
+    public float timePerDecrement;
     float timer = 0.0f;
     public bool work;
     public Transform fill;
 
     private void Start() {
         fill = transform.Find("Fill Parent");
+        if (work) {
+            timePerDecrement = 0.25f;
+        } else {
+            timePerDecrement = 1.0f;
+        }
     }
 
     public void increase(float f) {
@@ -30,7 +35,7 @@ public class Meter : MonoBehaviour {
 
     public void Update() {
         timer += Time.deltaTime;
-        if (timer > timePerDecrement && work) {
+        if (timer > timePerDecrement) {
             currentValue -= 0.01f;
             timer = 0.0f;
         }
