7455056c1cab122acff1bea1a0759112e8c5b153
wdhg <wdhgrant@gmail.com>
2020-07-12 18:25:34 +0100

    get timer working

---
+++ Assets/Game Manager/GameManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading;
+using TMPro;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+  public TextMeshProUGUI timerText;
+
+  private float time;
+
+  private void Update() {
+    if(Input.GetKeyDown(KeyCode.Escape)) {
+      Application.Quit();
+    }
+    time += Time.deltaTime;
+    timerText.text = "Timer - " + (Mathf.Round(time * 100) / 100).ToString();
+  }
+}
